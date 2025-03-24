9419dc4950705d33d45b0b3c83943cc73bafc180
Nived R S <nivedrsalini@gmail.com>
2025-03-24 23:47:07 +0530

    TCP

--- sockets/TCP/client.c
+++ sockets/TCP/client.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <string.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 
+#define BUFFER_SIZE 1024
 
 void main(){
   int client;
-  char buffer[1024];
+  char buffer[BUFFER_SIZE];
   struct sockaddr_in server_addr;
+  struct sockaddr_storage store;
+
   socklen_t addr_size;
 
   client = socket(AF_INET,SOCK_STREAM,0);
   server_addr.sin_family = AF_INET;
-  server_addr.sin_port   = htons(6265);
+  server_addr.sin_port = htons(5555);
   server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-  connect(client,(struct sockaddr*)&server_addr,sizeof(server_addr));
+  connect(client , (struct sockaddr *)&server_addr , sizeof(server_addr));
 
-  printf("[info] Sending data ...\n");
-
-  strcpy(buffer,"Hi this is client\n");
-  send(client,buffer,19,0);
-  recv(client,buffer,1024,0);
-  printf("[recv] Recieved : %s\n",buffer);
-
+  strcpy(buffer, "Hi Server !\n");
+  send(client,buffer,11,0);
+  recv(client,buffer,BUFFER_SIZE,0);
+  printf("Recieved : %s \n",buffer);
   close(client);
 }
--- sockets/TCP/server.c
+++ sockets/TCP/server.c
@@ -1,50 +1,48 @@
 #include <stdio.h>
-#include <sys/socket.h>   //to use sockets
-#include <netinet/in.h>   //contains definitions for IP family
 #include <string.h>
-#include <arpa/inet.h>    //contains internet operations
-#include <unistd.h>       //POSIX system APIs
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
 
 void main(){
-  int server,client;
-  char buffer[1024];
-
+  int server, client;
+  char buffer[BUFFER_SIZE];
   struct sockaddr_in server_addr;
   struct sockaddr_storage store;
-
   socklen_t addr_size;
 
-
   server = socket(AF_INET,SOCK_STREAM,0);
-  //AF_INET stands for Address FAMILY : IPV4
-  //SOCK_STEAM for TCP
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(5555);
+  server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+  bind(server,(struct sockaddr *)&server_addr,sizeof(server_addr));
+
+  int listen_status = listen(server,5);
+
+  if(!listen_status){
+    printf("Listening....\n");
+  }else{
+    printf("Unable to listen Exiting ....\n");
+    return;
+  }
+
+  client = accept(server , (struct sockaddr *)&store , &addr_size);
+
+  recv(client,buffer,BUFFER_SIZE,0);
+  printf("Recieved : %s \n",buffer);
   
-  server_addr.sin_family=AF_INET;
-  server_addr.sin_port=htons(6265);
-  server_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+  strcpy(buffer,"Hi,Client this is server\n\0");
+  printf("Sending Data to Client\n");
 
-
-  bind(server,(struct sockaddr*)&server_addr,sizeof(server_addr));
-  //File descriptor , server address pointer , address length
-
-  if(listen(server,5) == 0)
-    printf("[info] Listening\n");
-  else
-    printf("[err]  Cannot Listen\n");
-
-  client = accept(server,(struct sockaddr*)&store,&addr_size);
-  //socket descriptor , client storage address , size of address structure
-  
-  recv(client,buffer,1024,0);
-  printf("[recv] Data Recieved : %s\n",buffer);
-
-
-  strcpy(buffer,"Hi this is server\n");
-  printf("[info] Sending data\n");
-  send(client,buffer,19,0);
-
+  send(client,buffer,25,0);
 
   close(client);
   close(server);
+
+  return;
 }
 
